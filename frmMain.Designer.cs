c9dc456674737eeda2d0606f00ab7b9790e89667
root <mgiraudo@ies21.edu.ar>
2022-09-07 11:56:34 -0300

    Otra prueba

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -37,6 +37,7 @@
             this.rbAdmRed = new System.Windows.Forms.RadioButton();
             this.rbDesarrollo = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.gbOficio.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -142,11 +143,22 @@
             this.button1.Text = "xxxx";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(148, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "xxxx";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(485, 367);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.gbOficio);
             this.Controls.Add(this.btnRegistrar);
@@ -176,6 +188,7 @@
         private System.Windows.Forms.RadioButton rbAdmRed;
         private System.Windows.Forms.RadioButton rbDesarrollo;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
